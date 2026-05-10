37305acf8f54c415ef056027ea9864987830044c
seelny <a2cd85@mail.ru>
2026-05-10 13:03:13 +0000

    Initial commit: wrong input error handler (#1)

--- src/main/java/org/example/urlshortener/ErrorHandler.java
+++ src/main/java/org/example/urlshortener/ErrorHandler.java
@@ -1,11 +1,13 @@
 package org.example.urlshortener;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ErrorHandler {
@@ -14,5 +16,15 @@ public class ErrorHandler {
     public ResponseEntity<ErrorResponse> errorCather(UrlNotFoundException exception){
         ErrorResponse urlNotFound = new ErrorResponse(LocalDateTime.now(), 404,  "URL not found");
         return ResponseEntity.status(404).body(urlNotFound);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException exception){
+        exception.getBindingResult().getFieldErrors();
+        String errorMessage = exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),400, errorMessage);
+        return ResponseEntity.status(400).body(errorResponse);
     }
 }
